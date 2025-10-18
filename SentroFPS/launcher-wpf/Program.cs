2c8e6ade16f545f406910fe8196b935f04b1b349
sentrodev <sentrorp@gmail.com>
2025-10-18 22:03:33 +0200

    Update Program.cs

--- SentroFPS/launcher-wpf/Program.cs
+++ SentroFPS/launcher-wpf/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Windows;
 
 namespace SentroFPS.Launcher
@@ -8,9 +9,13 @@ namespace SentroFPS.Launcher
         [STAThread]
         public static void Main()
         {
+            File.WriteAllText("sentro_debug.txt", $"[{DateTime.Now}] Entrée dans Main()\n");
+
             var app = new App();
             var window = new MainWindow();
             app.Run(window);
+
+            File.AppendAllText("sentro_debug.txt", $"[{DateTime.Now}] Fenêtre affichée\n");
         }
     }
 }
